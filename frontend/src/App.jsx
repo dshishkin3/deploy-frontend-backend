7981fd97c14f655aad5e7175c031f9dfcc0e3889
dshishkin543 <79608355+dshishkin3@users.noreply.github.com>
2024-09-20 15:26:40 +0300

    fix: change axios api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,45 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [items, setItems] = useState(null)
+  const [items, setItems] = useState(null);
 
   const fetchItems = () => {
-    axios.get('https://api.site-test-deploy1.ru/items').then(r => {
-      setItems(r.data)
-    })
-  }
+    axios.get("/api/items").then((r) => {
+      setItems(r.data);
+    });
+  };
 
   useEffect(() => {
-    fetchItems()
+    fetchItems();
     setInterval(() => {
-      fetchItems()
-    }, 2000)
-  }, [])
+      fetchItems();
+    }, 2000);
+  }, []);
 
   return (
     <>
-      {items && items.map(item => {
-        return <span style={{padding:'0px 4px'}} key={item.name} className="roll-out">
-          <img src={item.img} alt='logo' width="16" style={{padding:'0px 5px'}}></img>
-          <span>{item.name}</span>
-        </span>
-      })}
+      {items &&
+        items.map((item) => {
+          return (
+            <span
+              style={{ padding: "0px 4px" }}
+              key={item.name}
+              className="roll-out"
+            >
+              <img
+                src={item.img}
+                alt="logo"
+                width="16"
+                style={{ padding: "0px 5px" }}
+              ></img>
+              <span>{item.name}</span>
+            </span>
+          );
+        })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
